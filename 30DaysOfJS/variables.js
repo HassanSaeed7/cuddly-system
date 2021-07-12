bc92f600a60d235594be34ce16460edbd01c93c9
HassanSaeed7 <42257531+HassanSaeed7@users.noreply.github.com>
2021-07-12 13:09:32 -0700

    Add files via upload

---
+++ 30DaysOfJS/variables.js
@@ -0,0 +1,41 @@
+//comments can make code readable
+
+//Welcome to 30DaysofJavaScript
+
+/*comments can make code readable,
+easy to reuse and informative*/
+
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let firstName = 'Hassan' //string
+let male = true //boolean
+let partnerName //undefined
+let javaScriptSkills = null //null... for now
+
+//Declare four variables without assigning values
+let firstVariable
+let secondVariable
+let thirdVariable
+let fourthVariable
+
+//Declare four variables with assigned values
+let fifthVariable = 1
+let sixthVariable = 2
+let seventhVariable = 3
+let eighthVariable = 4
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let lastName = 'Saeed'
+let maritalStatus = 'single'
+let country = 'USA'
+let age = 27
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName0 = 'Hassan', 
+lastName0 = 'Saeed', 
+maritalStatus0 = 'single', 
+country0 = 'USA', 
+age0 = 27
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 27
+let yourAge = 30
