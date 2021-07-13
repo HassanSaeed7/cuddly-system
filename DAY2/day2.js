1c137113f6d26603bf62d12c249e9e49c417f3bd
HassanSaeed7 <42257531+HassanSaeed7@users.noreply.github.com>
2021-07-12 17:19:26 -0700

    Add files via upload

---
+++ DAY2/day2.js
@@ -0,0 +1,14 @@
+//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'
+let challenge = '30 days of JavaScript'
+
+//Print the string on the browser console using console log
+console.log(challenge)
+
+//Print the length of the string on the browser console using console
+console.log(challenge.length)
+
+//Change all the string characters to capital letters using toUpperCase() method
+console.log(challenge.toUpperCase())
+
+//All lower case now
+console.log(challenge.toLowerCase())
